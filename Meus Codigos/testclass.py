3b49b7758c4dbecd58b3a15b5bb4b36e7f365878
Gustavo Rodrigues <guriogrande75@gmail.com>
2026-04-25 15:16:10 -0300

    Update testclass.py

--- Meus Codigos/testclass.py
+++ Meus Codigos/testclass.py
@@ -1,6 +1,8 @@
 from rich.console import Console
 from rich.panel import Panel
 
-console = Console()
+# Força o sistema de cores (color_system pode ser "standard", "256" ou "truecolor")
+console = Console(force_terminal=True, color_system="truecolor")
+
 console.print("[bold red]Teste de Cor[/] funcionando!")
 console.print(Panel("Painel Colorido", style="on blue", title="Sucesso"))
