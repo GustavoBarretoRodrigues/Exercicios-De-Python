9e9632b2c75b40223be90c03de82a952df6254f3
Gustavo Rodrigues <guriogrande75@gmail.com>
2026-04-25 14:55:43 -0300

    Test de class

--- Meus Codigos/testclass.py
+++ Meus Codigos/testclass.py
@@ -1,18 +1,6 @@
-class MinhaClassDocumentada:
-    """Está é a documentação da minha class, como exemplo do __doc__"""
+from rich.console import Console
+from rich.panel import Panel
 
-    def __init__(self, name=''):
-        self.name = name
-
-    def __str__(self):
-        return 'Hello, World !'
-
-
-mcd = MinhaClassDocumentada("Test")
-print(mcd)
-
-
-print(MinhaClassDocumentada.__doc__)
-print(mcd.__dict__)
-print(mcd.__getstate__())
-print(mcd.__class__)
+console = Console()
+console.print("[bold red]Teste de Cor[/] funcionando!")
+console.print(Panel("Painel Colorido", style="on blue", title="Sucesso"))
